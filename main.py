adbc99d205248fdd3684e5a3be9ada4e75cac554
Michael Woon <michael_woon17@yahoo.com>
2018-10-16 13:00:09 -0400

    tested message counting

--- main.py
+++ main.py
@@ -1,3 +1,17 @@
 #fb-info-parser
+import json
+#https://matplotlib.org/users/installing.html
+
+with open('messages\hle.json', 'r') as f:
+    jsondict = json.load(f)
 
 #count messages for each person
+mw = 0
+hl = 0
+for item in jsondict['messages']:
+    if item['sender_name'] == 'Hayden Le':
+        hl+=1
+    if item['sender_name'] == 'Michael Woon':
+        mw+=1
+print(hl)
+print(mw)
