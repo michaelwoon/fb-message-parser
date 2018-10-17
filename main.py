2cb5091703c659d28d436e9f105107e176df6e0a
Michael Woon <michael_woon17@yahoo.com>
2018-10-16 23:38:41 -0400

    Created function

--- main.py
+++ main.py
@@ -1,17 +1,28 @@
 #fb-info-parser
 import json
+from datetime import datetime
 #https://matplotlib.org/users/installing.html
 
-with open('messages\hle.json', 'r') as f:
-    jsondict = json.load(f)
-
 #count messages for each person
-mw = 0
-hl = 0
-for item in jsondict['messages']:
-    if item['sender_name'] == 'Hayden Le':
-        hl+=1
-    if item['sender_name'] == 'Michael Woon':
-        mw+=1
-print(hl)
-print(mw)
+def countoneperson(filepath,fullname):
+    with open(filepath, 'r') as f:
+        jsondict = json.load(f)
+    mw = 0
+    other = 0
+    for item in jsondict['messages']:
+        if item['sender_name'] == fullname:
+            other+=1
+        if item['sender_name'] == 'Michael Woon':
+            mw+=1
+    otherpercent = (other*100)/float(mw+other)
+    print "number of", fullname, "messages:", other
+    print "number of Michael messages:", mw
+    print otherpercent,"percent not michael"
+    return
+
+countoneperson('messages/tuan.json','Tuan Nguyen')
+
+#https://docs.python.org/3/library/datetime.html
+ts = int("1284101485")
+date = datetime.utcfromtimestamp(ts)
+print(date.month)
